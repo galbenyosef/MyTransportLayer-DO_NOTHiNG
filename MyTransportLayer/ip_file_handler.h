3bcbf7f010d4b23698a377bd7322c4d7c5719602
Gal <Gal@GAL>
2016-11-12 21:08:08 +0200

    added base class and functions signatures

---
+++ MyTransportLayer/ip_file_handler.h
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class ip_file_handler {
+
+
+
+public:
+
+	static bool file_to_ip_packets(std::string, std::string, unsigned int, unsigned int);
+	static bool ip_packets_to_file(std::string, std::string, unsigned int, unsigned int);
+
+
+};
+
+bool ip_file_handler::file_to_ip_packets(std::string input_file_name, std::string output_file_name, unsigned int source_ip, unsigned int destination_ip) {
+
+
+
+}
+
+bool ip_file_handler::ip_packets_to_file(std::string input_file_name, std::string output_file_name, unsigned int source_ip, unsigned int destination_ip) {
+
+
+
+}
---
+++ MyTransportLayer/main.cpp
@@ -0,0 +1,7 @@
+#include "ip_file_handler.h"
+
+int main() {
+
+	bool success = ip_file_handler::file_to_ip_packets("file1.jpg", "file1_ip.bin", 0x3234FF21, 0x3F34DF29);
+
+}
